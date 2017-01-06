ec07607916509654a15efc972f359e94ebe13ba4
Dyl89 <dylukyanov@gmail.com>
2017-01-06 22:14:50 +0300

    Operations Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,19 @@
 public class Calculate {
 
-	public static void main(String[] args) {
+	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int st = Integer.valueOf(arg[2]);
+		int summ = first + second;
+		int minus = first - second;
+		int del = first / second;
+		int umn = first * second;
+		int step = (int)Math.pow(first, st);
+		System.out.println("Sum " + summ);
+		System.out.println("minus " + minus);
+		System.out.println("Del " + del);
+		System.out.println("Umn " + umn);
+		System.out.println("step " + step);
 	}
-
 }
